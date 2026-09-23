77e3d1666eea7a439c9605526a79d88c704506fc
AntonioJan Cabuenas <antoniojan.cabuenas@cit.edu>
2026-09-23 20:28:07 +0800

    Add minimum late fee - cabuenas.antoniojan

--- catalog.js
+++ catalog.js
@@ -8,6 +8,6 @@ function calculateLateFee(daysLate, ratePerDay) {
   }
 
   const fee = Math.round(daysLate * ratePerDay);
-  return Math.min(fee, 20);
+  return Math.min(Math.max(fee, 1), 20);
 }
 module.exports = { isValidLoan, calculateLateFee };
